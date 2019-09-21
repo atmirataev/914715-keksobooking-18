064e4656edd426523d942a18bc435790f5d22964
Atmir Ataev <atmirataev@gmail.com>
2019-09-21 12:59:12 +0300

    Геренацию объявления вынесли в отдельную функцию

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 var NUMBER_OF_ADS = 8;
 
-var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var HOTEL_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN_CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var HOTEL_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var HOTEL_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 var getRandomElem = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
@@ -18,8 +18,7 @@ var adTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
 
-var renderAd = function (index) {
-  var adElem = adTemplate.cloneNode(true);
+var createAd = function (index) {
   var ad = {
     author: {
       avatar: 'img/avatars/user0' + index + '.png',
@@ -29,14 +28,14 @@ var renderAd = function (index) {
       title: null,
       address: null,
       price: null,
-      type: getRandomElem(TYPES),
+      type: getRandomElem(HOTEL_TYPES),
       rooms: null,
       guests: null,
       checkin: getRandomElem(CHECKIN_CHECKOUT_TIME),
       checkout: getRandomElem(CHECKIN_CHECKOUT_TIME),
-      features: getRandomElem(FEATURES),
+      features: getRandomElem(HOTEL_FEATURES),
       description: null,
-      photos: getRandomElem(PHOTOS),
+      photos: getRandomElem(HOTEL_PHOTOS),
     },
 
     location: {
@@ -45,9 +44,14 @@ var renderAd = function (index) {
     }
   };
 
-  adElem.style = 'left: ' + ad.location.x + 'px; top: ' + ad.location.y + 'px';
-  adElem.img.src = ad.author.avatar;
-  adElem.alt = ad.offer.title;
+  return ad;
+};
+
+var renderAd = function (index) {
+  var adElem = adTemplate.cloneNode(true);
+  adElem.style = 'left: ' + createAd().location.x + 'px; top: ' + createAd().location.y + 'px';
+  adElem.img.src = createAd(index).author.avatar;
+  adElem.alt = createAd().offer.title;
 
   return adElem;
 };
@@ -55,7 +59,7 @@ var renderAd = function (index) {
 var mapPinsList = document.querySelector('.map__pins');
 var fragment = document.createDocumentFragment();
 
-for (var i = 0; i <= NUMBER_OF_ADS; i++) {
+for (var i = 1; i <= NUMBER_OF_ADS; i++) {
   fragment.appendChild(renderAd(i));
   mapPinsList.appendChild(fragment);
 }

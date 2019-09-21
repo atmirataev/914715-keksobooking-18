22cf92857a05fcd4856495e1a77cae37d9dc5721
Atmir Ataev <atmirataev@gmail.com>
2019-09-21 17:17:47 +0300

    Исправляет ошибки в д/з №3/2

--- js/main.js
+++ js/main.js
@@ -1,32 +1,36 @@
 'use strict';
 
-var NUMBER_OF_ADS = 8;
-
+var COUNT_OF_ADS = 8;
+var MIN_X = 0;
+var MAX_X = 1200;
+var MIN_Y = 130;
+var MAX_Y = 630;
 var HOTEL_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN_CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
 var HOTEL_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var HOTEL_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var adTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var map = document.querySelector('.map');
+
+map.classList.remove('map--faded');
 
 var getRandomElem = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var getRandomNumber = function (min, max) {
+  var rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
 
-var adTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
-
-var createAd = function (index) {
-  var ad = {
+var createAdvertisement = function (index) {
+  var advertisement = {
     author: {
       avatar: 'img/avatars/user0' + index + '.png',
     },
 
     offer: {
       title: null,
-      address: null,
+      address: '600, 350',
       price: null,
       type: getRandomElem(HOTEL_TYPES),
       rooms: null,
@@ -35,31 +39,36 @@ var createAd = function (index) {
       checkout: getRandomElem(CHECKIN_CHECKOUT_TIME),
       features: getRandomElem(HOTEL_FEATURES),
       description: null,
-      photos: getRandomElem(HOTEL_PHOTOS),
+      photos: 'http://o0.github.io/assets/images/tokyo/hotel' + getRandomNumber(1, 3) + '.jpg',
     },
 
     location: {
-      x: null,
-      y: null,
+      x: getRandomNumber(MIN_X, MAX_X),
+      y: getRandomNumber(MIN_Y, MAX_Y),
     }
   };
 
-  return ad;
+  return advertisement;
 };
 
-var renderAd = function (index) {
+var renderAdvertisement = function (index) {
   var adElem = adTemplate.cloneNode(true);
-  adElem.style = 'left: ' + createAd().location.x + 'px; top: ' + createAd().location.y + 'px';
-  adElem.img.src = createAd(index).author.avatar;
-  adElem.alt = createAd().offer.title;
+  adElem.style.left = createAdvertisement().location.x + 'px';
+  adElem.style.top = createAdvertisement().location.y + 'px';
+  adElem.querySelector('img').src = createAdvertisement(index).author.avatar;
+  adElem.querySelector('img').alt = createAdvertisement().offer.title;
 
   return adElem;
 };
 
-var mapPinsList = document.querySelector('.map__pins');
-var fragment = document.createDocumentFragment();
+var showAdvertisements = function () {
+  var mapPinsList = document.querySelector('.map__pins');
+  var fragment = document.createDocumentFragment();
 
-for (var i = 1; i <= NUMBER_OF_ADS; i++) {
-  fragment.appendChild(renderAd(i));
-  mapPinsList.appendChild(fragment);
-}
+  for (var i = 1; i <= COUNT_OF_ADS; i++) {
+    fragment.appendChild(renderAdvertisement(i));
+    mapPinsList.appendChild(fragment);
+  }
+};
+
+showAdvertisements();

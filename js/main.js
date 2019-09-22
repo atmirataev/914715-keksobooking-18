1cd1a551e84aba74fa2d44525c0319a241a5ad9e
Atmir Ataev <atmirataev@gmail.com>
2019-09-22 11:42:21 +0300

    Исправляет "address" и "photos" в д/з №3/2

--- js/main.js
+++ js/main.js
@@ -8,21 +8,39 @@ var MAX_Y = 630;
 var HOTEL_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN_CHECKOUT_TIME = ['12:00', '13:00', '14:00'];
 var HOTEL_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var HOTEL_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var adTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var map = document.querySelector('.map');
 
 map.classList.remove('map--faded');
 
+/**
+ * @param {array} arr массив данных для генерации случайных данных
+ * @return {string} случайный элемент массива
+ */
 var getRandomElem = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
+/**
+ * @param {number} min от
+ * @param {number} max до
+ * @return {number} случайное число от min до max
+ */
 var getRandomNumber = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 };
 
+HOTEL_PHOTOS.length = getRandomNumber(1, 3);
+
+/**
+ * @param {number} index сюда передается индекс из цикла, который находится в showAdvertisements()
+ * @return {string} структура объявления без данных
+ */
 var createAdvertisement = function (index) {
+  var locationX = getRandomNumber(MIN_X, MAX_X);
+  var locationY = getRandomNumber(MIN_Y, MAX_Y);
   var advertisement = {
     author: {
       avatar: 'img/avatars/user0' + index + '.png',
@@ -30,7 +48,7 @@ var createAdvertisement = function (index) {
 
     offer: {
       title: null,
-      address: '600, 350',
+      address: locationX + ' ,' + locationY,
       price: null,
       type: getRandomElem(HOTEL_TYPES),
       rooms: null,
@@ -39,18 +57,22 @@ var createAdvertisement = function (index) {
       checkout: getRandomElem(CHECKIN_CHECKOUT_TIME),
       features: getRandomElem(HOTEL_FEATURES),
       description: null,
-      photos: 'http://o0.github.io/assets/images/tokyo/hotel' + getRandomNumber(1, 3) + '.jpg',
+      photos: HOTEL_PHOTOS,
     },
 
     location: {
-      x: getRandomNumber(MIN_X, MAX_X),
-      y: getRandomNumber(MIN_Y, MAX_Y),
+      x: locationX,
+      y: locationY,
     }
   };
 
   return advertisement;
 };
 
+/**
+ * @param {number} index сюда передается индекс из цикла, который находится в showAdvertisements()
+ * @return {string} объявление с данными
+ */
 var renderAdvertisement = function (index) {
   var adElem = adTemplate.cloneNode(true);
   adElem.style.left = createAdvertisement().location.x + 'px';
@@ -61,6 +83,9 @@ var renderAdvertisement = function (index) {
   return adElem;
 };
 
+/**
+ * @description показывает объявления на карте
+ */
 var showAdvertisements = function () {
   var mapPinsList = document.querySelector('.map__pins');
   var fragment = document.createDocumentFragment();
